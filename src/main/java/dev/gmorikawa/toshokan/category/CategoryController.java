580c4cee0a30b18ac0c54c3af0c03f77e455ba87
gmorikawa <morikawa.gabriel@outlook.jp>
2025-04-06 11:42:29 +0900

    Category entity basic CRUD implemented

--- src/main/java/dev/gmorikawa/toshokan/category/CategoryController.java
+++ src/main/java/dev/gmorikawa/toshokan/category/CategoryController.java
@@ -2,7 +2,12 @@ package dev.gmorikawa.toshokan.category;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +23,26 @@ public class CategoryController {
     @GetMapping()
     public List<Category> getAll() {
         return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Category getById(@PathVariable String id) {
+        return service.getById(id);
+    }
+
+    @PostMapping()
+    public Category create(@RequestBody Category category) {
+        return service.insert(category);
+    }
+
+    @PatchMapping("/{id}")
+    public Category update(@PathVariable String id, @RequestBody Category category) {
+        return service.update(id, category);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean remove(@PathVariable String id) {
+        return service.remove(id);
     }
 
 }
--- src/main/java/dev/gmorikawa/toshokan/category/CategoryService.java
+++ src/main/java/dev/gmorikawa/toshokan/category/CategoryService.java
@@ -15,4 +15,29 @@ public class CategoryService {
     public List<Category> getAll() {
         return repository.findAll();
     }
+
+    public Category getById(String id) {
+        return repository.getReferenceById(id);
+    }
+
+    public Category insert(Category entity) {
+        return repository.save(entity);
+    }
+
+    public Category update(String id, Category entity) {
+        Category current = getById(id);
+
+        if(current != null) {
+            current.setName(entity.getName());
+        }
+
+        return repository.save(current);
+    }
+
+    public boolean remove(String id) {
+        Category entity = getById(id);
+        repository.delete(entity);
+
+        return true;
+    }
 }
