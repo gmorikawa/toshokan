169045376c375f46882b965af1a9462f2bbe4003
gmorikawa <morikawa.gabriel@outlook.jp>
2025-04-20 18:30:43 +0900

    Basic tests on controllers initialization and User Service

---
+++ src/test/java/dev/gmorikawa/toshokan/SmokeTest.java
@@ -0,0 +1,45 @@
+package dev.gmorikawa.toshokan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import dev.gmorikawa.toshokan.auth.AuthenticationController;
+import dev.gmorikawa.toshokan.document.book.BookController;
+import dev.gmorikawa.toshokan.document.whitepaper.WhitepaperController;
+import dev.gmorikawa.toshokan.publisher.PublisherController;
+import dev.gmorikawa.toshokan.topic.TopicController;
+import dev.gmorikawa.toshokan.user.UserController;
+
+@SpringBootTest
+public class SmokeTest {
+
+    @Autowired
+    UserController userController;
+
+    @Autowired
+    BookController bookController;
+
+    @Autowired
+    WhitepaperController whitepaperController;
+
+    @Autowired
+    PublisherController publisherController;
+
+    @Autowired
+    TopicController topicController;
+
+    @Autowired
+    AuthenticationController authenticationController;
+
+    @Test
+    void contextLoads() throws Exception {
+        assert userController != null : "UserController bean should be loaded";
+        assert bookController != null : "BookController bean should be loaded";
+        assert whitepaperController != null : "WhitepaperController bean should be loaded";
+        assert publisherController != null : "PublisherController bean should be loaded";
+        assert topicController != null : "TopicController bean should be loaded";
+        assert authenticationController != null : "AuthenticationController bean should be loaded";
+    }
+
+}
---
+++ src/test/java/dev/gmorikawa/toshokan/user/UserServiceTest.java
@@ -0,0 +1,40 @@
+package dev.gmorikawa.toshokan.user;
+
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import dev.gmorikawa.toshokan.user.enumerator.UserRole;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+    @Mock
+    private UserRepository repository;
+
+    @InjectMocks
+    private UserService service;
+
+    @Test
+    public void testGetUsers() {
+        List<User> list = new ArrayList<User>();
+        list.add(new User("test1", "user1", "password1", "user1@email.com", UserRole.ADMIN, "User Uno"));
+        list.add(new User("test2", "user2", "password2", "user2@email.com", UserRole.LIBRARIAN, "User Dos"));
+        list.add(new User("test3", "user3", "password3", "user3@email.com", UserRole.READER, "User Tres"));
+        list.add(new User("test4", "user4", "password4", "user4@email.com", UserRole.READER, "User Cuatro"));
+
+        Mockito.when(repository.findAll()).thenReturn(list);
+
+        List<User> users = service.getUsers();
+
+        assert users.size() == 4 : "Expected 4 users, but got " + users.size();
+        Mockito.verify(repository, Mockito.times(1)).findAll();
+    }
+}
