89e1334ea28b4837dc367ee02025de7491c93dfa
gmorikawa <morikawa.gabriel@outlook.jp>
2025-04-21 14:41:56 +0900

    CORS configuration to allow requests with required METHODS

--- src/main/java/dev/gmorikawa/toshokan/ToshokanApplication.java
+++ src/main/java/dev/gmorikawa/toshokan/ToshokanApplication.java
@@ -18,7 +18,11 @@ public class ToshokanApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/api/**")
+                    .allowedOrigins("http://localhost:4200")
+                    .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
             }
         };
     }
