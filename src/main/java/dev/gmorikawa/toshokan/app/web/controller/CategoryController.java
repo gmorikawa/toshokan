7cc9db340fcceddda53fadc2623e6d0ee5b9b6e6
gmorikawa <morikawa.gabriel@outlook.jp>
2025-09-24 09:51:10 +0900

    CRUD pages for Category context

---
+++ src/main/java/dev/gmorikawa/toshokan/app/web/controller/CategoryController.java
@@ -0,0 +1,79 @@
+package dev.gmorikawa.toshokan.app.web.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import dev.gmorikawa.toshokan.app.web.shared.Meta;
+import dev.gmorikawa.toshokan.app.web.shared.Page;
+import dev.gmorikawa.toshokan.domain.category.Category;
+import dev.gmorikawa.toshokan.domain.category.CategoryService;
+
+@Controller("web.category")
+@RequestMapping(path = "categories")
+public class CategoryController {
+
+    private final CategoryService service;
+
+    public CategoryController(CategoryService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Category> categories = service.getAll();
+        
+        model.addAttribute("meta", new Meta("List Categories || Toshokan"));
+        model.addAttribute("page", new Page("List Categories"));
+        model.addAttribute("categories", categories);
+
+        return "category/list";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("meta", new Meta("Create Category || Toshokan"));
+        model.addAttribute("page", new Page("Create Category"));
+        model.addAttribute("category", new Category());
+
+        return "category/create";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String update(@PathVariable String id, Model model) {
+        Category category = service.getById(id);
+
+        model.addAttribute("meta", new Meta("Update Category || Toshokan"));
+        model.addAttribute("page", new Page("Update Category"));
+        model.addAttribute("category", category);
+
+        return "category/update";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute Category category) {
+        service.create(category);
+
+        return "redirect:/categories/list";
+    }
+
+    @PostMapping("/{id}/update")
+    public String update(@PathVariable String id, @ModelAttribute Category category) {
+        service.update(id, category);
+
+        return "redirect:/categories/list";
+    }
+
+    @GetMapping("/{id}/remove")
+    public String remove(@PathVariable String id, @ModelAttribute Category category) {
+        service.remove(id);
+
+        return "redirect:/categories/list";
+    }
+}
